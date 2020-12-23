f8bb6c11227245d01ea5e7c814d68f525fdef4c5
enganddeveloper <46913754+EngandDeveloper@users.noreply.github.com>
2020-12-23 00:00:41 -0500

    Initial IndexDB client side database code

--- subwebsites/ProudActivity/js/list.js
+++ subwebsites/ProudActivity/js/list.js
@@ -34,7 +34,52 @@ function addTask(){
     }
 }
 
-//function checked(){
-//    taskText.
-//}
+// ******** Client Side Database (IndexDB) Code ***********
+// Mozilla developer guide for indexDB was followed, for future reference the guide link:
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Client-side_storage
+
+
+//Initiate the database: db
+let db;
+
+
+
+//On Window Open
+window.onload = function() {
+    //request database
+    let request = window.indexedDB.open('taskList', 1);
+
+    //error handling for db request
+    request.onerror = function() {
+        console.log('Database failed to open');
+    };
+
+    //when the request is successful:
+    request.onsuccess = function(){
+        console.log('Database opened successfully');
+
+        //assign request to db
+        db = request.result;
+
+        //display data by calling a function
+        //displayTasks();
+    }
+
+    //databse set-up and modification(updgrade)
+    request.onupgradeneeded = function(e) {
+        //grab a reference to the opened database
+        let db = e.target.result;
+
+        //create an objectStore which will act like a single db table to store data
+        //think keyPath as primarykey in db
+        let objectStore = db.createObjectStore('tasksOS', {keyPath: 'taskId', autoIncrement:true});
+
+        //Define which items will be stored in objectStore, think them as attributes in db
+        //In our case we only need task:String to store the TODO item apart from the unique id which we defined above
+        objectStore.createIndex('task', 'task', {unique:false});
+
+        console.log('Database setup complete');
+    }
+
+}
 
